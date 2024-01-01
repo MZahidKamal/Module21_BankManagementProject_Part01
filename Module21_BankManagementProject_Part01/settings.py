e032b00b2f891a7c048999e4a3563fb268aa401c
MZahidKamal <kamal.md.zahid@gmail.com>
2024-01-01 21:18:48 +0100

    Daily Learning & Commit

--- Module21_BankManagementProject_Part01/settings.py
+++ Module21_BankManagementProject_Part01/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://mamar-bank-d8qe.onrender.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://mamar-bank-new.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
