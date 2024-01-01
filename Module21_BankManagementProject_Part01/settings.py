173a82de7a504f998cac9737b57cf957c92e93f3
MZahidKamal <kamal.md.zahid@gmail.com>
2024-01-01 18:41:01 +0100

    Daily Learning & Commit

--- Module21_BankManagementProject_Part01/settings.py
+++ Module21_BankManagementProject_Part01/settings.py
@@ -163,7 +163,7 @@ MEDIA_ROOT = BASE_DIR
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Django to Gmail :: Email sending configuration
-EMAIL_BACKEND = 'django.learningCBVs_app.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
